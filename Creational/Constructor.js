4491042657738c723188c10a2178a720716871e9
Nguyễn Mạnh Hiệp <nguyen.fs90@gmail.com>
2020-10-23 22:59:02 +0900

    constructor pattern

---
+++ Creational/Constructor.js
@@ -0,0 +1,12 @@
+class Hero {
+    constructor(name, specialAbility){
+        this._name = name;
+        this._specialAbility = specialAbility;
+
+        this.getDetails = function() {
+            return `${this._name} can ${this._specialAbility}`;
+        };
+    }
+}
+
+module.exports = Hero;
---
+++ index.js
@@ -0,0 +1,3 @@
+const hero = require('./Creational/Constructor');
+const a = new hero();
+console.log(a);
